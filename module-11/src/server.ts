06faee3b4ff0d3bbe5bdad8ab58e2853248140d2
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-12 21:56:22 +0600

    Working with Environment-Based Configuration

--- module-11/src/server.ts
+++ module-11/src/server.ts
@@ -1,4 +1,5 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
@@ -17,6 +18,6 @@ const server: Server = http.createServer(
 );
 
 
-server.listen(5000, () => {
-    console.log(`server is running on port : ${5000}`);
+server.listen(config.port, () => {
+    console.log(`server is running on port : ${config.port}`);  
 })
