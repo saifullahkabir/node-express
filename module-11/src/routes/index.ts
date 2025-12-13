06a96bc6cf288094cc0fe8e8fa74a4be79e4b246
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-14 01:22:19 +0600

    Setting Up a Fake JSON Database

--- module-11/src/routes/index.ts
+++ module-11/src/routes/index.ts
@@ -1,3 +1,4 @@
+import { readUsers, writeUsers } from "../helpers/fileDB";
 import parseBody from "../helpers/parseBody";
 import addRoutes from "../helpers/RouteHandler";
 import sendJson from "../helpers/sendJson";
@@ -16,8 +17,20 @@ addRoutes("GET", "/api", (req, res) => {
   });
 });
 
-addRoutes("POST", "/api/users", async(req, res) => {
-    const body = await parseBody(req);
-    console.log(body);
-    sendJson(res, 201, {success: true, data :body} );
-})
+addRoutes("POST", "/api/users", async (req, res) => {
+  const body = await parseBody(req);
+
+  // user json read
+  const users = readUsers();
+
+  const newUser = {
+    id: Date.now(),
+    ...body,
+  };
+
+  users.push(newUser);
+
+  writeUsers(users);    
+
+  sendJson(res, 201, { success: true, data: body });
+});
