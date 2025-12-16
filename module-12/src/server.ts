a8a1014a315602a44ba74505a53b3c44bcc4ef5e
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-16 19:37:51 +0600

    Create our first User using POST

--- module-12/src/server.ts
+++ module-12/src/server.ts
@@ -50,8 +50,28 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello next level developers!");
 });
 
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+    console.log(result.rows[0]);
+
+    res.status(201).json({
+      success: true,
+      message: "Data Inserted Successfully",
+      data: result.rows[0]
+    })
+    
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 
   res.status(201).json({
     success: true,
