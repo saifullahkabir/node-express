c7fd9ac08edd874e366b79878fbb41e4af122361
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-16 23:51:15 +0600

    post a todos data in db

--- module-12/src/server.ts
+++ module-12/src/server.ts
@@ -50,7 +50,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello next level developers!");
 });
 
-// users CRUD
+//* users CRUD
 //* save a user data in db
 app.post("/users", async (req: Request, res: Response) => {
   const { name, email } = req.body;
@@ -178,6 +178,31 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
     res.status(500).json({
       success: false,
       message: err.message,
+    });
+  }
+});
+
+//* TODOS CRUD
+//* post a todos data in db
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Todo created",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
     });
   }
 });
