c1dee0fb8a583e42705e20c012ffd67e00a34f82
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-18 02:50:14 +0600

    Cleaning server.ts with user.routes.ts

---
+++ module-13/src/modules/user/user.routes.ts
@@ -0,0 +1,49 @@
+import { Request, Response, Router } from "express";
+import { pool } from "../../config/db";
+
+const router = Router();
+
+//* save a user data in db
+router.post("/", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+    // console.log(result.rows[0]);
+
+    res.status(201).json({
+      success: true,
+      message: "Data Inserted Successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//* get all users data
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+});
+
+export const userRoutes = router;
--- module-13/src/server.ts
+++ module-13/src/server.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import config from "./config";
 import initDB, { pool } from "./config/db";
+import logger from "./middleware/logger";
+import { userRoutes } from "./modules/user/user.routes";
 
 const app = express();
 const port = config.port;
@@ -11,59 +13,13 @@ app.use(express.json());
 // initializing DB
 initDB();
 
-// logger middleware
-const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
-  next();
-};
-
 app.get("/", logger, (req: Request, res: Response) => {
   res.send("Hello next level developers!");
 });
 
 //* users CRUD
-//* save a user data in db
-app.post("/users", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
+app.use("/users", userRoutes);
 
-  try {
-    const result = await pool.query(
-      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
-      [name, email]
-    );
-    // console.log(result.rows[0]);
-
-    res.status(201).json({
-      success: true,
-      message: "Data Inserted Successfully",
-      data: result.rows[0],
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
-
-//* get all users data
-app.get("/users", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(`SELECT * FROM users`);
-
-    res.status(200).json({
-      success: true,
-      message: "Users retrieved successfully",
-      data: result.rows,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-      details: err,
-    });
-  }
-});
 
 //* get a specific user data
 app.get("/users/:id", async (req: Request, res: Response) => {
@@ -245,10 +201,10 @@ app.put("/todos/:id", async (req: Request, res: Response) => {
 
     if (result.rows.length === 0) {
       res.status(404).json({
-        success: false, 
-        message: "Todo not found!!!"
-      })
-    } else{
+        success: false,
+        message: "Todo not found!!!",
+      });
+    } else {
       res.status(200).json({
         success: true,
         message: "Updated todo successfully",
@@ -289,7 +245,6 @@ app.delete("/todos/:id", async (req: Request, res: Response) => {
     });
   }
 });
-
 
 //* not found route
 app.use((req, res) => {
