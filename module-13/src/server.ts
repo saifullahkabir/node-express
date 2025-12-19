008764938a33acbeb7adecfdf244063e33dcddf8
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-20 01:37:43 +0600

    Creating Password Field & Hashing Passwords

--- module-13/src/server.ts
+++ module-13/src/server.ts
@@ -4,6 +4,7 @@ import initDB, { pool } from "./config/db";
 import logger from "./middleware/logger";
 import { userRoutes } from "./modules/user/user.routes";
 import { todoRoutes } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
 
 const app = express();
 const port = config.port;
@@ -24,7 +25,8 @@ app.use("/users", userRoutes);
 //* TODOS CRUD
 app.use("/todos", todoRoutes);
 
-
+//* auth routes
+app.use("/auth", authRoutes);
 
 //* not found route
 app.use((req, res) => {
